d20f66126637b8e25045f0cff64748b27844776b
danwilliams201302-max <daniel@lyonshaulage.com>
2026-08-20 00:50:05 +0100

    Enable cross-tab planner allocation sync

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { MsalProvider } from '@azure/msal-react';
 import { PublicClientApplication } from '@azure/msal-browser';
 import { App } from './App';
 import './runtimeGuards';
+import './orderPlanningSyncBridge';
 import './fleetioSyncPatch';
 import './masterDataCleanupFetchPatch';
 import './operationalPresentationPatch';
