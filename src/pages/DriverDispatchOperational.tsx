0c5d4a2e946320628f7a5cc5b195490406ead0ec
danwilliams201302-max <daniel@lyonshaulage.com>
2026-09-10 10:31:41 +0100

    feat: add driver and subbie actions to dispatch

--- src/pages/DriverDispatchOperational.tsx
+++ src/pages/DriverDispatchOperational.tsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import { BackloadMatchNotifications } from "../components/BackloadMatchNotifications";
 import { CustomerLoadPlanActions } from "../components/CustomerLoadPlanActions";
 import { DispatchBoard } from "../components/dispatch/DispatchBoard";
+import { DispatchResourceQuickAdd } from "../components/dispatch/DispatchResourceQuickAdd";
 import "../authoritative-dispatch.css";
+import "../dispatch-resource-quick-add.css";
 
 function currentDispatchDate() {
   return new URLSearchParams(window.location.search).get("date") || new Date().toISOString().slice(0, 10);
@@ -10,6 +12,7 @@ function currentDispatchDate() {
 
 export function DriverDispatchOperational() {
   const [dispatchDate, setDispatchDate] = useState(currentDispatchDate);
+  const [dispatchRevision, setDispatchRevision] = useState(0);
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
@@ -20,9 +23,13 @@ export function DriverDispatchOperational() {
   return <>
     <BackloadMatchNotifications />
     <DispatchBoard
+      key={`${dispatchDate}-${dispatchRevision}`}
       planningDate={dispatchDate}
       onPlanningDateChange={setDispatchDate}
-      extraActions={<CustomerLoadPlanActions date={dispatchDate} />}
+      extraActions={<>
+        <CustomerLoadPlanActions date={dispatchDate} />
+        <DispatchResourceQuickAdd onSaved={() => setDispatchRevision(value => value + 1)} />
+      </>}
     />
   </>;
 }
