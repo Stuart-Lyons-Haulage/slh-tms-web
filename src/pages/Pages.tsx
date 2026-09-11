aafc429dd84ae9942f6c54400c4e943e37b4e92a
danwilliams201302-max <daniel@lyonshaulage.com>
2026-09-11 20:24:51 +0100

    fix(master-data): correct fuel export component import

--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useMemo, useRef, useState, type FormEvent, type ReactNode } from 'react';
-import { MasterDataExportButton } from './MasterDataExportButton';
+import { MasterDataExportButton } from '../components/MasterDataExportButton';
 import * as XLSX from 'xlsx';
 import { api, type Customer, type CustomerContact, type DiagnosticsTables, type Driver, type DriverAssignment, type FleetStatus, type Load, type LoadDispatch, type MarketContact, type ReturnLoadSuggestions, type Site, type StageBatchRequest, type StagedImport, type Telemetry, type Trailer, type TransportOrder, type Vehicle } from '../lib/api';
 import { useAccessToken } from '../lib/auth';
